b7b9530905d3c1ce2a0f0f75ebfa2698a21d9e6b
scao <sihao.cao@gmail.com>
2020-05-11 16:26:59 -0400

    a prototype todo list with the ability to check and uncheck todo items

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import ListItem from './ListItem';
 import './App.css';
+import _ from 'lodash';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    chores: [
+      {
+        id: 1,
+        name: 'Take out trash',
+        description: 'Trash removal from both bins',
+        completed: true
+      },
+      {
+        id: 2,
+        name: 'Do the dishes',
+        description: 'Wash and dry the kitchen',
+        completed: false
+      },
+      {
+        id: 3,
+        name: 'Walk the dog',
+        description: 'Take dog around the block 2 times',
+        completed: true
+      }
+    ]
+  };
+
+  handleOnClick = id => {
+    const chores = _.cloneDeep(this.state.chores);
+
+    for (let chore of chores) {
+      if (chore.id === id) {
+        chore.completed = !chore.completed;
+        break;
+      }
+    }
+
+    this.setState({ chores });
+  };
+
+  render() {
+    const { chores } = this.state;
+    return (
+      <div className='App'>
+        <h1>Chores</h1>
+        <ul>
+          {chores.map(chore => (
+            <ListItem
+              key={chore.id}
+              id={chore.id}
+              name={chore.name}
+              completed={chore.completed}
+              description={chore.description}
+              handleOnClick={this.handleOnClick}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
